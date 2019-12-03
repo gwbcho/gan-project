4f804e67665495cc7b91f136796cc65a083c9333
gwbcho <gregwbcho@gmail.com>
2019-12-02 20:37:57 -0500

    GAN-000 Squash this.

--- assignment.py
+++ assignment.py
@@ -262,21 +262,27 @@ def train(generator, discriminator, dataset_iterator, manager):
     for iteration, batch in enumerate(dataset_iterator):
         # TODO: Train the model
         noise = tf.Variable(tf.random.uniform([args.batch_size, args.z_dim]))
-        with tf.GradientTape(persistent=True) as gen_tape, tf.GradientTape() as disc_tape:
+        with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
             gen_output = generator(noise)
             disc_real_output = discriminator(batch)
             disc_fake_output = discriminator(gen_output)
             gen_loss = generator.loss_function(disc_fake_output)
             disc_loss = discriminator.loss_function(disc_real_output, disc_fake_output)
-
+        gen_grads = gen_tape.gradient(gen_loss, generator.trainable_variables)
         disc_grads = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
         # apply back propagation using determined gradients and the model optimizer
         discriminator.optimizer.apply_gradients(zip(disc_grads, discriminator.trainable_variables))
         # update generator multiple times
         for _ in range(args.num_gen_updates):
-            gen_grads = gen_tape.gradient(gen_loss, generator.trainable_variables)
-            generator.optimizer.apply_gradients(zip(gen_grads, generator.trainable_variables))
-        del gen_tape
+            noise = tf.Variable(tf.random.uniform([args.batch_size, args.z_dim]))
+            with tf.GradientTape() as gen_tape:
+                gen_output = generator(noise)
+                disc_real_output = discriminator(batch)
+                disc_fake_output = discriminator(gen_output)
+                gen_loss = generator.loss_function(disc_fake_output)
+                disc_loss = discriminator.loss_function(disc_real_output, disc_fake_output)
+                gen_grads = gen_tape.gradient(gen_loss, generator.trainable_variables)
+                generator.optimizer.apply_gradients(zip(gen_grads, generator.trainable_variables))
 
         # Save
         if iteration % args.save_every == 0:
