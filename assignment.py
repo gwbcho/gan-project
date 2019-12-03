GAN-000 Fixed gradient values.

assignment.py
@@ -263,11 +263,10 @@ def train(generator, discriminator, dataset_iterator, manager):
         # Train the model
         if iteration % args.num_gen_updates == 0:
             noise = tf.Variable(tf.random.uniform([args.batch_size, args.z_dim]))
+            gen_output = generator(noise)
             with tf.GradientTape() as disc_tape:
-                gen_output = generator(noise)
                 disc_real_output = discriminator(batch)
                 disc_fake_output = discriminator(gen_output)
-                gen_loss = generator.loss_function(disc_fake_output)
                 disc_loss = discriminator.loss_function(disc_real_output, disc_fake_output)
             disc_grads = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
             # apply back propagation using determined gradients and the model optimizer
@@ -278,10 +277,8 @@ def train(generator, discriminator, dataset_iterator, manager):
         noise = tf.Variable(tf.random.uniform([args.batch_size, args.z_dim]))
         with tf.GradientTape() as gen_tape:
             gen_output = generator(noise)
-            disc_real_output = discriminator(batch)
             disc_fake_output = discriminator(gen_output)
             gen_loss = generator.loss_function(disc_fake_output)
-            disc_loss = discriminator.loss_function(disc_real_output, disc_fake_output)
         gen_grads = gen_tape.gradient(gen_loss, generator.trainable_variables)
         generator.optimizer.apply_gradients(zip(gen_grads, generator.trainable_variables))
 